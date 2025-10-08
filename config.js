f158f1bca70890d2ffe190d27bef4a03d733f0b5
Morpho Monarchy Studios <jhward0810@gmail.com>
2025-10-08 17:44:56 -0600

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
     description:"A hot-reload particle effects library for GameMaker 2024.11 (and later)",
   latestVersion:"1.0.0",
   favicon:"assets/docsIcon.png",
-  themeColor:"#1C1B21",
   otherVersions:[""],
+  themeColor:"#1C1B21",
   name:"Pollen",
 };
